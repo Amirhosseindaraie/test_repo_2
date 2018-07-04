aeda0dfa953f40f2aef801dd2f0b345b25c6f8a6
DaraieAmirH <daraieamirh@gmail.com>
2018-07-04 19:28:49 +0430

    Second commit

--- code.cpp
+++ code.cpp
@@ -2,5 +2,7 @@
 
 int main(int argc, char const *argv[]) {
 	std::cerr << "a message form me :-)" << std::endl;
+	std::cerr << "a message form me22222 just added :-)" << std::endl;
+	
 	return 0;
 }
